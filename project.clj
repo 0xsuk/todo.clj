eec9becfb00c16498f9438190931428d84962141
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-11-27 20:34:23 +0900

    middleware for better error and auto reload

--- project.clj
+++ project.clj
@@ -5,7 +5,12 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring "1.4.0"]
-                 [compojure "1.4.0"]]
+                 [compojure "1.4.0"]
+                 [environ "1.0.1"]]
   :main ^:skip-aot todo.clj
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :plugins [[lein-environ "1.0.1"]]
+  :profiles
+  {:uberjar {:aot :all}
+   :dev {:dependencies [[prone "0.8.2"]]
+         :env {:dev true}}})
--- src/todo/clj.clj
+++ src/todo/clj.clj
@@ -1,17 +1,27 @@
 (ns todo.clj
   (:require [compojure.core :refer [routes]]
             [ring.adapter.jetty :as server]
-            [ring.util.response :as res]
             [todo.handler.main :refer [main-routes]]
-            [todo.handler.todo :refer [todo-routes]])
+            [todo.handler.todo :refer [todo-routes]]
+            [todo.middleware :refer [wrap-dev]]
+            [environ.core :refer [env]])
   (:gen-class))
 
 (defonce server (atom nil))
 
+(defn- wrap [handler middleware opt]
+  (if (true? opt)
+    (middleware handler)
+    (if opt
+      (middleware handler opt)
+      handler)))
+
 (def app
-  (routes
-   todo-routes
-   main-routes))
+  (->
+   (routes
+    todo-routes
+    main-routes)
+   (wrap wrap-dev (:dev env))))
 
 (defn start-server []
   (when-not @server
@@ -30,4 +40,5 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Todo.clj!")
+  (start-server))
